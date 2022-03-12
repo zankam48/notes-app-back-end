7c65cd6e57a42f040cba0f12533e06afdcdeacec
zankam48 <hikamizan771@gmail.com>
2022-03-12 14:18:03 +0700

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,31 @@
+import {addNoteHandler, deleteNoteByIdHandler, editNoteByIdHandler, getAllNotesHandler, getNoteByIdHandler} from './handler.js';
+
+const routes = [
+    {
+        method: 'POST',
+        path: '/notes',
+        handler: addNoteHandler
+    },
+    {
+        method: 'GET',
+        path: '/notes',
+        handler: getAllNotesHandler
+    },
+    {
+        method: 'GET',
+        path: '/notes/{id}',
+        handler: getNoteByIdHandler
+    },
+    {
+        method: 'PUT',
+        path: '/notes/{id}',
+        handler: editNoteByIdHandler
+    },
+    {
+        method: 'DELETE',
+        path: '/notes/{id}',
+        handler: deleteNoteByIdHandler
+    }
+];
+
+export default routes;
